ab5fd59097b35e7c5923ef470a4274d9fdf41ed9
Tuğçe Yenisey Erkan <yeniseys16@gmail.com>
2021-06-05 01:25:38 +0300

    serving an existing member is done

--- frontend/Add_Grantor.php
+++ frontend/Add_Grantor.php
@@ -1,3 +1,55 @@
+<?php
+include '../database/config.php';
+$conn = OpenCon();
+
+$username = $password = $check = '';        // initialize with empty string
+$errors = array('grantor_name' => '', 'phone_number' => ''
+, 'book_code' => '', 'book_name' => '', 'book_name' => '', 'book_name' => '', 'book_name' => ''); // keys and their ampty values
+
+
+session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // username and password sent from form 
+
+    if (empty($_POST['username'])) { 
+        $errors['username'] = 'An username is required';
+    } else {
+        $username = mysqli_real_escape_string($conn, $_POST['username']);
+    }
+
+    if (empty($_POST['password'])) {
+        $errors['password'] = 'A password is required';
+    } else {
+        $password = mysqli_real_escape_string($conn, $_POST['password']);
+    }
+
+
+    $table_name = "employees";
+    $page_uri = "DatabasesProject-2021/frontend/mainpage.php";
+
+
+    if (!empty($_POST['password']) && !empty($_POST['username'])) {
+        $sql = 'SELECT Employee_id FROM '.$table_name.' WHERE Username = "'.$username.'" and password = "'.$password.'"';
+
+        $result = mysqli_query($conn, $sql);
+    
+                
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+        $count = mysqli_num_rows($result);
+        if ($count == 1) {
+            $_SESSION['Username'] = $username;
+            header("location: http://localhost/" . $page_uri);
+        } else {
+            $errors['check'] = "Your Login Username or Password is invalid";
+        }
+    }
+    
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,34 +60,6 @@
     <link rel="stylesheet" href="grantors.css">
     <title>Grantors</title>
 </head>
-
-<script>
-    // Get the modal
-    var modal = document.getElementById("myModal");
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-</script>
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Domine&display=swap');
@@ -132,36 +156,36 @@
                 <label>Grantor Information:</label><br>
                 <hr>
                 <label id="text_input">Grantor Name:</label>
-                <input type="text" name="" id="select"><br>
+                <input type="text" name="grantor_name" id="select"><br>
                 <br>
                 <label id="text_input">Grantor Phone Number:</label>
-                <input type="text" name="" id="select"><br>
+                <input type="text" name="phone_number" id="select"><br>
                 <br><br>
                 <label>Donated Book Information:</label><br>
                 <hr>
                 <label id="text_input">Book Code:</label>
-                <input type="text" name="" id="select"><br>
+                <input type="text" name="book_code" id="select"><br>
                 <br>
                 <label id="text_input"><label id="text_input">Book Name:</label>
-                <input type="text" name="" id="select"><br>
+                <input type="text" name="book_name" id="select"><br>
                 <br>
                 <label id="text_input">Author:</label>
-                <input type="text" name="" id="select"><br>
+                <input type="text" name="author" id="select"><br>
                 <br>
                 <label id="text_input">Type:</label>
-                <input type="text" name="" id="select"><br>
+                <input type="text" name="type" id="select"><br>
                 <br>
                 <label id="text_input">Number Of Edition:</label>
-                <input type="text" name="" id="select"><br>
+                <input type="text" name="edition" id="select"><br>
                 <br>
                 <label id="text_input">Quantity:</label>
-                <input type="number" name="" id="select" min="1"><br>
+                <input type="number" name="quantity" id="select" min="1"><br>
                 <br>
                 <label id="text_input">Date</label>
-                <input type="date" name="" id="select"><br>
+                <input type="date" name="date" id="select"><br>
                 <br>
 
-                <button id="submit" type="submit">Add</button>
+                <button id="submit" name="submit" type="submit">Add</button>
 
             </form>
         </div>
---
+++ frontend/serve_existing_member.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="members.css?v=<?php echo time(); ?>">
+    <title>Serve to Member</title>
+</head>
+
+<?php
+include '../database/config.php';
+$conn = OpenCon();
+
+$username = '';
+session_start();
+
+$username = $_SESSION['Username'];
+$sql = 'SELECT * FROM employees WHERE Username = "' . $username . '"';
+$query = mysqli_query($conn, $sql);
+$result = mysqli_fetch_array($query);
+$emp=$result['Employee_Id'];
+
+$sqlm = 'SELECT * FROM members';
+$querym = mysqli_query($conn, $sqlm);
+$rowsm = array();
+while ($resultm = mysqli_fetch_array($querym)) {
+    $rowsm[] = $resultm;
+}
+
+$sqlb = 'SELECT * FROM books';
+$queryb = mysqli_query($conn, $sqlb);
+$rowsb = array();
+while ($resultb = mysqli_fetch_array($queryb)) {
+    $rowsb[] = $resultb;
+}
+
+
+$mname = $bname = $bcode = $date = '';        // initialize with empty string
+$errors = array('m_name' => '', 'b_name' => '', 'date' => '', 'check' => ''); // keys and their ampty values
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // username and password sent from form 
+
+    if (empty($_POST['m_name'])) {
+        $errors['m_name'] = 'A member name is required';
+    } else {
+        $mname = mysqli_real_escape_string($conn, $_POST['m_name']);
+    }
+
+    if (empty($_POST['b_name'])) {
+        $errors['b_name'] = 'A book name is required';
+    } else {
+        $bname = mysqli_real_escape_string($conn, $_POST['b_name']);
+    }
+
+    if (empty($_POST['date'])) {
+        $errors['date'] = 'A date is required';
+    } else {
+        $date = mysqli_real_escape_string($conn, $_POST['date']);
+    }
+
+    if (!empty($_POST['m_name']) && !empty($_POST['b_name']) && !empty($_POST['date'])) {
+
+
+        $sql1 = 'SELECT * FROM members WHERE Member_Name = "' . $mname . '"';
+        $query1 = mysqli_query($conn, $sql1);
+        $result1 = mysqli_fetch_array($query1);
+
+
+        $sql2 = 'SELECT * FROM books WHERE Book_Name = "' . $bname . '"';
+        $query2 = mysqli_query($conn, $sql2);
+        $result2 = mysqli_fetch_array($query2);
+
+        $sql4 = 'UPDATE books SET Quantity=Quantity-1 WHERE Book_Name = "' . $bname . '"';
+
+        $sql3 = "INSERT INTO borrowed_books ( Book_Id, Member_Id, Borrow_Date) 
+        VALUES ( '" . $result2['Book_Id'] . "', '" . $result1['Member_Id'] . "', '$date');";
+
+        $sql5 = "INSERT INTO serves (Member_Id, Employee_Id) 
+        VALUES ( '" . $result1['Member_Id'] . "', '$emp');";
+
+        if (mysqli_query($conn, $sql3) && mysqli_query($conn, $sql4) && mysqli_query($conn, $sql5)) {
+            echo '<script> alert("Book served to member successfully."); // window.location="serve_existing_member.php" </script>';
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+    }
+}
+?>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Domine&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Stalemate&display=swap');
+
+    body {
+        font-family: 'Domine', serif;
+    }
+
+    strong {
+        font-family: 'Domine', serif;
+        font-size: 25px;
+    }
+
+    body b {
+        font-family: 'Stalemate', cursive;
+        font-size: 50px;
+    }
+</style>
+
+<body>
+    <!-- Side navigation -->
+    <div class="sidenav">
+        <div><img src="../frontend/assets/logo.png" height="150px" style="opacity: 0.8;"></img>
+        </div>
+
+        <br>
+        <strong style="text-align:center;"><b style="font-size: 70px;">Welcome</b> Luna Lovegood</strong>
+
+        <br>
+        <hr style="border-color: white;">
+
+        <table id="tablenavbar">
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/mainpage.php"></a>
+                </td>
+
+                <td> <a id="icon1" href="http://localhost/DatabasesProject-2021/frontend/mainpage.php">Main Page</a>
+                </td>
+            </tr>
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/books.php"></a>
+                </td>
+
+                <td> <a id="icon2" href="http://localhost/DatabasesProject-2021/frontend/books.php">Books</a>
+                </td>
+            </tr>
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/members.php"></a>
+                </td>
+
+                <td> <a id="icon3" href="http://localhost/DatabasesProject-2021/frontend/members.php">Members</a>
+                </td>
+            </tr>
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/grantors.php"></a>
+                </td>
+
+                <td> <a id="icon3" href="http://localhost/DatabasesProject-2021/frontend/grantors.php">Grantors</a>
+                </td>
+            </tr>
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/employees.php"></a>
+                </td>
+
+                <td> <a id="icon3" href="http://localhost/DatabasesProject-2021/frontend/employees.php">Employees</a>
+                </td>
+            </tr>
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/login.php"></a>
+                </td>
+
+                <td><a id="icon4" href="http://localhost/DatabasesProject-2021/frontend/login.php">Log Out</a></td>
+            </tr>
+        </table>
+
+
+    </div>
+
+    <div class="main">
+
+        <div class="Instructors_requests" id="Serve">
+            <h1 style="text-align: center;">Serve to The Member</h1>
+            <hr><br>
+
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+
+                <label>Member Information:</label><br>
+                <hr>
+                <label id="text_input">Member Name:</label>
+
+                <select name="m_name" class="select" value="<?php echo htmlspecialchars($mname); ?>">
+                        <?php
+                        foreach ($rowsm as $rowm) {
+                            echo '
+                        <option> ' . $rowm['Member_Name'] . ' </option>
+                        ';
+                        }
+                        ?>
+                    </select>
+               <br>
+                <div style="color: red;">
+                    <?php echo $errors['m_name']; ?>
+                    <!-- display error message here !-->
+                </div>
+                <br><br>
+                <label>Borrowed Book Information:</label><br>
+                <hr>
+                <label id="text_input"><label id="text_input">Book Name:</label>
+                <select name="b_name" class="select" value="<?php echo htmlspecialchars($mname); ?>">
+                        <?php
+                        foreach ($rowsb as $rowb) {
+                            echo '
+                        <option> ' . $rowb['Book_Name'] . ' </option>
+                        ';
+                        }
+                        ?>
+                    </select>
+                    <br>
+                    <div style="color: red;">
+                        <?php echo $errors['b_name']; ?>
+                        <!-- display error message here !-->
+                    </div>
+                    <br>
+                    <label id="text_input">Date</label>
+                    <input type="date" name="date" id="select" value="<?php echo htmlspecialchars($date); ?>">
+                    <div style="color: red;">
+                        <?php echo $errors['date'];?>
+                        <!-- display error message here !-->
+                    </div>
+                    <br>
+                    <br>
+                    <button id="submit" type="submit">Serve</button>
+
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
