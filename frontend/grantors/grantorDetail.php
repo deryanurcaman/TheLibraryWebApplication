74169917e7ad8e3bb5acec0579cb30d50382b039
franazerek <rana.zerek@gmail.com>
2021-06-16 18:59:47 +0300

    Creating detail files

---
+++ frontend/grantors/grantorDetail.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="grantors.css?v=<?php echo time(); ?>">
+    <title>Grantors Detailed Info</title>
+</head>
+
+<script>
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Domine&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Stalemate&display=swap');
+
+    body {
+        font-family: 'Domine', serif;
+    }
+
+    strong {
+        font-family: 'Domine', serif;
+        font-size: 25px;
+    }
+
+    body b {
+        font-family: 'Stalemate', cursive;
+        font-size: 50px;
+    }
+</style>
+
+<body>
+    <!-- Side navigation -->
+    <div class="sidenav">
+    <div><img src="../../assets/logo.png" height="150px" style="opacity: 0.8;"></img>
+        </div>
+
+        <br>
+        <strong style="text-align:center;"><b style="font-size: 70px;">Welcome</b><br><?php echo $result2['Employee_Name']; ?></strong>
+
+        <br>
+        <hr style="border-color: white;">
+
+        <table id="tablenavbar">
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/mainpage.php"></a>
+                </td>
+
+                <td> <a id="icon1" href="http://localhost/DatabasesProject-2021/frontend/mainpage.php">Main Page</a>
+                </td>
+            </tr>
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/books/books.php"></a>
+                </td>
+
+                <td> <a id="icon2" href="http://localhost/DatabasesProject-2021/frontend/books/books.php">Books</a>
+                </td>
+            </tr>
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/members/members.php"></a>
+                </td>
+
+                <td> <a id="icon3" href="http://localhost/DatabasesProject-2021/frontend/members/members.php">Members</a>
+                </td>
+            </tr>
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/grantors/grantors.php"></a>
+                </td>
+
+                <td> <a id="icon3" href="http://localhost/DatabasesProject-2021/frontend/grantors/grantors.php">Grantors</a>
+                </td>
+            </tr>
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/employees/employees.php"></a>
+                </td>
+
+                <td> <a id="icon3" href="http://localhost/DatabasesProject-2021/frontend/employees/employees.php">Employees</a>
+                </td>
+            </tr>
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/login.php"></a>
+                </td>
+
+                <td><a id="icon4" href="http://localhost/DatabasesProject-2021/frontend/login.php">Log Out</a></td>
+            </tr>
+        </table>
+
+
+
+    </div>
+
+    <div class="main">
+        <div class="Instructors_requests">
+            <h1>Beatrice Haywood</h1><br>
+            <table>
+                <tr id="heads">
+                    <th id="hashtag">#</th>
+                    <th>Book Code</th>
+                    <th>Book Name</th>
+                </tr>
+                <?php $j = 1;
+                //foreach ($rows as $row) {}
+                ?>
+                    <tr>
+                
+                        <td id="hashtag"><?php echo $j ?></td>
+                        <td>BK20</td>
+                        <td>Chess Story</td>
+                        <!--<td><?php echo $row["Grantor_Code"]; ?></td>!-->
+                        
+                    </tr>
+                <?php $j = $j + 1;
+                ?>
+            </table>
+        </div>
+        <br><br>
+
+
+    </div>
+
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+
+        <!-- Modal content -->
+        <div class="modal-content">
+            <span class="close">&times;</span> Some text in the Modal..
+        </div>
+
+    </div>
+
+</body>
+
+</html>
---
+++ frontend/members/memberDetail.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="members.css?v=<?php echo time(); ?>">
+    <title>Members Detailed Info</title>
+</head>
+
+<script>
+    function save() {
+        alert("The Request Is Successfully Sent");
+    }
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Domine&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Stalemate&display=swap');
+
+    body {
+        font-family: 'Domine', serif;
+    }
+
+    strong {
+        font-family: 'Domine', serif;
+        font-size: 25px;
+    }
+
+    body b {
+        font-family: 'Stalemate', cursive;
+        font-size: 50px;
+    }
+</style>
+
+<body>
+    <!-- Side navigation -->
+    <div class="sidenav">
+        <div><img src="../../assets/logo.png" height="150px" style="opacity: 0.8;"></img>
+        </div>
+
+        <br>
+        <strong style="text-align:center;"><b style="font-size: 70px;">Welcome</b><br><?php echo $result2['Employee_Name']; ?></strong>
+
+        <br>
+        <hr style="border-color: white;">
+
+        <table id="tablenavbar">
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/mainpage.php"></a>
+                </td>
+
+                <td> <a id="icon1" href="http://localhost/DatabasesProject-2021/frontend/mainpage.php">Main Page</a>
+                </td>
+            </tr>
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/books/books.php"></a>
+                </td>
+
+                <td> <a id="icon2" href="http://localhost/DatabasesProject-2021/frontend/books/books.php">Books</a>
+                </td>
+            </tr>
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/members/members.php"></a>
+                </td>
+
+                <td> <a id="icon3" href="http://localhost/DatabasesProject-2021/frontend/members/members.php">Members</a>
+                </td>
+            </tr>
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/grantors/grantors.php"></a>
+                </td>
+
+                <td> <a id="icon3" href="http://localhost/DatabasesProject-2021/frontend/grantors/grantors.php">Grantors</a>
+                </td>
+            </tr>
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/employees/employees.php"></a>
+                </td>
+
+                <td> <a id="icon3" href="http://localhost/DatabasesProject-2021/frontend/employees/employees.php">Employees</a>
+                </td>
+            </tr>
+            <tr id="hv">
+                <td>
+                    <a href="http://localhost/DatabasesProject-2021/frontend/login.php"></a>
+                </td>
+
+                <td><a id="icon4" href="http://localhost/DatabasesProject-2021/frontend/login.php">Log Out</a></td>
+            </tr>
+        </table>
+
+
+    </div>
+
+    <div class="main">
+
+        <div class="Instructors_requests">
+            <h1>Jae Kim</h1><br>
+            <table>
+                <tr id="heads">
+                    <th id="hashtag">#</th>
+                    <th>Book Name</th>
+                    <th>Borrow Date</th>
+                    <th>Borrowing Process Employee</th>
+                    <th>Return Date</th>
+                    <th>Returning Process Employee</th>
+                </tr>
+                <?php $j = 1;
+                //foreach ($rows as $row) {}
+                ?>
+                    <tr>
+                        <td><?php echo $j ?></td>
+                        <td>Chess Story</td>
+                        <td>17.06.2021</td>
+                        <td>Barnaby Lee</td>
+                        <td>23.06.2021</td>
+                        <td>Tulip Karasu</td>
+                        <!--<td><?php echo $row["Grantor_Code"]; ?></td>!-->
+            
+                    </tr>
+                <?php $j = $j + 1;
+                 ?>
+            </table>
+
+        </div>
+    </div>
+</body>
+
+</html>
